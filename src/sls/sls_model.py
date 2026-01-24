c04e6805144566174c9fa015f33b5c2385ecadfb
dzuris <adamdzurilla19@gmail.com>
2026-01-24 23:55:51 +0100

    explanation files are derived

--- src/sls/sls_model.py
+++ src/sls/sls_model.py
@@ -15,21 +15,35 @@ class SlsExplainer(): # pylint: disable=too-few-public-methods
         self.output_file = os.path.join(output_folder, filename)
 
         # Initialize the explanation generator
-        self.generator = ExplanationGenerator(model_type=config.get("model_type", "nn"))
+        model_type = config.get("model_type", "nn")
+        self.generator = ExplanationGenerator(model_type=model_type)
+
+        # Obtain configuration properties for building the file names
+        explanations_folder = config.get('folder_with_explanations', "tmp/exp")
+        target_column = config['target_column']
 
         # SHAP
-        shap_file = config['shap']['file']
-        shap_metadata = config['shap']['metadata_file']
+        shap_file = os.path.join(
+            explanations_folder, f"{model_type}_shap_values_{target_column}.npz"
+        )
+        shap_metadata = os.path.join(
+            explanations_folder, f"{model_type}_shap_metadata_{target_column}.json"
+        )
         if shap_file and shap_metadata:
             self.generator.load_shap_data(shap_file, shap_metadata)
 
         # LIME
-        lime_file = config['lime']['file']
+        lime_instance_index = config['lime_instance_index']
+        lime_file = os.path.join(
+            explanations_folder, f"{model_type}_lime_{target_column}_{lime_instance_index}.json"
+        )
         if lime_file:
             self.generator.load_lime_explanation(lime_file)
 
         # Summarization
-        summarization_file = config['summarization']['file']
+        summarization_file = os.path.join(
+            explanations_folder, f"{model_type}_summarize_{target_column}.json"
+        )
         if summarization_file:
             self.generator.load_model_summary(summarization_file)
 
@@ -46,3 +60,11 @@ class SlsExplainer(): # pylint: disable=too-few-public-methods
             The full explanation text
         """
         return self.explanation_text
+
+    def get_output_file(self) -> str:
+        """Get the output filename.
+        
+        Returns:
+            The output file name
+        """
+        return self.output_file
